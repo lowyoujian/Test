b6bb1fb6a5b3ea6a7a8dc92cc102197c9f7a32b5
YouJian <lowyoujian@gmail.com>
2014-04-11 01:44:40 +0800

    aa
    aa

---
+++ ConsoleApplication8/ConsoleApplication1.UnitTest/Class1Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FirstApplication;
+
+namespace ConsoleApplication8Test
+{
+    
+
+    class Class1Test
+    {
+        private User user;
+
+        [SetUp]
+        public void Setup()
+        {
+            user = new User("as");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestRegisterAccount(string name)
+        {
+            user.RegisterAccount();
+
+        }
+    }
+}
---
+++ ConsoleApplication8/ConsoleApplication8/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication
+{
+    public class User
+    {
+        private string name;
+        public void RegisterAccount()
+        {
+            //Text from textfield are added to the object variables    
+            //Account stored in a file
+           
+            try
+            {
+                if (((this.name).Trim()).Length < 3
+                   || ((this.name).Trim()).Length > 20
+                   )
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+
+
+            catch (Exception e)
+            {
+                Console.WriteLine("{0} Exception caught.", e);
+            }
+
+            
+
+        }
+        public User(string name)
+        {
+            this.name = name;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Ls");
+            user.RegisterAccount();
+
+            Console.ReadKey();
+        }
+    }
+}
