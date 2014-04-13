22016efde47e06e25c7fa10b5ce5e5f08c7b7913
YouJian <lowyoujian@gmail.com>
2014-04-14 00:26:21 +0800

    fixed user registration

--- ConsoleApplication8/ConsoleApplication8/Program.cs
+++ ConsoleApplication8/ConsoleApplication8/Program.cs
@@ -78,28 +78,23 @@ namespace FirstApplication
         }
         static void Main(string[] args)
         {
-            string temp;
+            string choice;
             Console.WriteLine("Welcome to this hotel");
             Console.WriteLine("Are you a new user?(Y/N)");
-            temp = Console.ReadLine();
-            while (temp.ToUpper() != "Y" || temp.ToUpper() != "N")
+            choice = Console.ReadLine();
+            while (choice.ToUpper() != "Y" || choice.ToUpper() != "N")
             {
-                if (temp.ToUpper() == "Y")
-                {
-                    Menu.NewUser();
-                    Menu.DisplayRoom();
-                }
-                else if (temp.ToUpper() == "N")
-                {
-                    Menu.AlreadyUser();
-                    Menu.DisplayRoom();
-                }
+                if (choice.ToUpper() == "Y")
+                { Menu.NewUser();}
+                else if (choice.ToUpper() == "N")
+                {Menu.AlreadyUser();   }
                 else
                 {
                     Console.WriteLine("Invalid input. Please try again. Y/N ?");
-                    temp = Console.ReadLine();
+                    choice = Console.ReadLine();
                 }
             }
+            Menu.DisplayRoom();
 
             Console.ReadKey();
         }
@@ -162,21 +157,30 @@ namespace FirstApplication
 
         public static void NewUser()
         {
+            bool valid = true ;
             User user = new User();
-            Console.WriteLine("Please enter your desired username");
-            user.Name = Console.ReadLine();
-            Console.WriteLine("Please enter your desired password");
-            user.Password = Console.ReadLine();
-            Console.WriteLine("");
-            if (user.ValidateInput())
+            do
             {
-                user.RegisterAccount();
-                Console.WriteLine("You are now logged in");
-                Console.WriteLine("Press any key to continue");
-                Console.ReadKey();
-                Console.Clear();
-            }
-
+                Console.WriteLine("Please enter your desired username. (must contain at least 3 and max of 20 characters");
+                user.Name = Console.ReadLine();
+                Console.WriteLine("Please enter your desired password. (must contain at least 7 characters)");
+                user.Password = Console.ReadLine();
+                Console.WriteLine("");
+                valid = user.ValidateInput();// Returns false if doesn't meet criteria
+                if (valid)
+                {
+                    user.RegisterAccount();
+                    Console.WriteLine("You are now logged in");
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Username or password do not match criteria. Please try again.");
+                }
+            } while (!valid );
 
 
         }
@@ -201,14 +205,14 @@ namespace FirstApplication
 
         public static void DisplayRoom()
         {
-            string temp;
+            string choice;
 
 
             Reservation reservation1 = new Reservation();
             Console.WriteLine("Would you be interested in one of our Packages like our bundle of 5 Luxury Rooms and 5 Normal Rooms for only half of its original price?");
             Console.WriteLine("Y/N?");
-            temp = Console.ReadLine();
-            if (temp.ToUpper() == "Y")
+            choice = Console.ReadLine();
+            if (choice.ToUpper() == "Y")
             {
                 // Constructors call Factory method
                 Package[] packages = new Package[2];
@@ -219,8 +223,8 @@ namespace FirstApplication
 
                 Console.WriteLine("1) FiveLuxuryAndNormalRoom \n 2)TwentyNormalRoom \n 3) back");
                 Console.WriteLine("choose 1");
-                temp = Console.ReadLine();
-                if (temp == "1")
+                choice = Console.ReadLine();
+                if (choice == "1")
                 {
                     // Calling Factory CreateRoom Method Method                  
                     foreach (Room room in packages[0].Rooms)
@@ -230,14 +234,14 @@ namespace FirstApplication
                     reservation1.RoomList.Concat(packages[0].Rooms);
                     Selection();
                 }
-                if (temp == "2")
+                if (choice == "2")
                 {
                     reservation1.RoomList.AddRange(packages[1].Rooms);
                     Selection();
                 }
             }
 
-            else if (temp.ToUpper() == "N")
+            else if (choice.ToUpper() == "N")
             {
                 LuxuryRoom luxroom1 = new LuxuryRoom();
                 NormalRoom normalroom1 = new NormalRoom();
